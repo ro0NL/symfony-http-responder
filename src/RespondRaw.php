659c2a28090b52f5f4ebdc33a928204a14222010
Roland Franssen <franssen.roland@gmail.com>
2019-03-05 11:56:37 +0100

    add raw respond type

---
+++ src/RespondRaw.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ro0NL\HttpResponder;
+
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @author Roland Franssen <franssen.roland@gmail.com>
+ */
+final class RespondRaw extends Respond
+{
+    /**
+     * @var string
+     */
+    public $contents;
+
+    public function __construct(string $contents)
+    {
+        $this->contents = $contents;
+    }
+}
