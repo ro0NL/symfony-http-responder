d2f1ce6657a4715a437b781b9b4305e90fa99bd7
Roland Franssen <franssen.roland@gmail.com>
2019-03-05 11:41:21 +0100

    tweak respond type

--- src/Respond.php
+++ src/Respond.php
@@ -4,10 +4,55 @@ declare(strict_types=1);
 
 namespace ro0NL\HttpResponder;
 
+use Symfony\Component\HttpFoundation\Response;
+
 /**
  * @author Roland Franssen <franssen.roland@gmail.com>
  */
-interface Respond
+abstract class Respond
 {
+    /**
+     * @var int
+     */
+    public $status = Response::HTTP_OK;
 
+    /**
+     * @var string[]|string[][]
+     */
+    public $headers = [];
+
+    /**
+     * @var string[]|string[][]
+     */
+    public $flashes = [];
+
+    public function withStatus(int $status): self
+    {
+        $respond = clone $this;
+        $respond->status = $status;
+
+        return $respond;
+    }
+
+    /**
+     * @param string[]|string[][] $headers
+     */
+    public function withHeaders(array $headers): self
+    {
+        $respond = clone $this;
+        $respond->headers = $headers;
+
+        return $respond;
+    }
+
+    /**
+     * @param string[]|string[][] $flashes
+     */
+    public function withFlashes(array $flashes): self
+    {
+        $respond = clone $this;
+        $respond->flashes = $flashes;
+
+        return $respond;
+    }
 }
